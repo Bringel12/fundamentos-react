f6ab981818bc8fee3447eaf69fc85e98ac1554d9
Bringel12 <matheushenriquebringelferreira@gmail.com>
2026-03-09 11:40:47 -0300

    refereciando_um_arquivo_css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import ReactDOM from "react-dom/client";
 
-const el = document.getElementById("root");
+// Seu código está correto para React 17 ou menor, mas o problema mais comum hoje é que o projeto está usando React 18 ou 19, onde ReactDOM.render() foi removido.
+// import ReactDOM from 'react-dom' import React from 'react' ReactDOM.render( <div>Olá React</div>, document.getElementById('root') )
+// Por isso o React não renderiza nada.
+// import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(el);
+// const el = document.getElementById("root");
 
-root.render("Olá !");
+// const root = ReactDOM.createRoot(el);
+
+// root.render("Olá !");
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const tag = <strong>Olá React!</strong>
+// ReactDOM.render(
+//     <div>
+//       {tag}
+//     </div>,
+//     document.getElementById('root')
+// );
+import'./index.css'
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const tag = <strong>Olá React</strong>;
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(
+    <div>
+        {tag}
+    </div>
+)
